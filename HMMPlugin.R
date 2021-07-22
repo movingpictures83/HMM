ba84d3c5673f2afbed06bf49097e406d285ce83a
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-07-22 14:16:44 -0400

    Initial

---
+++ HMMPlugin.R
@@ -0,0 +1,17 @@
+library(HDclust)
+
+input <- function(inputfile) {
+   dataX <<- read.csv(inputfile)
+   dataX <<- dataX[,-1]
+}   
+
+run <- function() {
+   Vb <- vb(1, dim=2, numst=1)
+set.seed(12345)
+modelBIC <- hmmvbBIC(dataX, VbStructure=Vb)
+clust <<- hmmvbClust(faithful, bicObj=modelBIC)
+}
+
+output <- function(outputfile) {
+   write.csv(clust@clsid, outputfile)
+}
